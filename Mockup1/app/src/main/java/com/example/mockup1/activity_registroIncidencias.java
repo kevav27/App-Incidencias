cae78f5a32e0dc247d376a66d4851525cf11594c
kevav27 <38163635+kevav27@users.noreply.github.com>
2019-11-01 20:00:44 -0600

    Add files via upload

---
+++ Mockup1/app/src/main/java/com/example/mockup1/activity_registroIncidencias.java
@@ -0,0 +1,11 @@
+package com.example.mockup1;
+
+import android.os.Bundle;
+
+public class activity_test {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
